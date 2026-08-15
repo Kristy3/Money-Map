7f07e69366589580130902c743e0f81b9705c4e3
Kristy3 <kristy@chromicky.com>
2026-08-16 09:15:57 +1000

    Add gaming purpose

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const initialState = {
   debts: [],
   rules: defaultRules,
   categories: ['Groceries', 'Gift Cards', 'Gifts', 'Holiday', 'Kids Sports', 'Work', 'Insurance', 'Fees', 'Car', 'One-Off', 'Wellness', 'Subscriptions', 'Utilities', 'Health', 'Transport', 'Dining', 'Income', 'Uncategorised'],
-  purposes: ['Household', 'General Spending', 'Wellness', 'Eating Out', 'Birthday', 'Sherwood', 'Dee Why', 'Berridale', 'Car', 'Amex Fees', 'Charging', 'Mobile Phone', 'Electricity', 'Parking', 'Servicing', 'Gym', 'AI', 'Canada Alaska', 'USA 2026', 'Queensland 2027', 'Europe 2027', 'Sport', 'Entertainment', 'Home', 'Getting Around', 'Income'],
+  purposes: ['Household', 'General Spending', 'Wellness', 'Eating Out', 'Birthday', 'Sherwood', 'Dee Why', 'Berridale', 'Car', 'Amex Fees', 'Charging', 'Mobile Phone', 'Electricity', 'Parking', 'Servicing', 'Gym', 'Gaming', 'AI', 'Canada Alaska', 'USA 2026', 'Queensland 2027', 'Europe 2027', 'Sport', 'Entertainment', 'Home', 'Getting Around', 'Income'],
   people: ['Connor', 'Family', 'Home', 'Personal', 'Martin', 'Adam', 'Jacob', 'Kristy'],
 };
 
